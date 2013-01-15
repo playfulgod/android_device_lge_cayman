c465944f29058f5780a06b624d19db68ff2cf6db
Tom Marshall <tdm.code@gmail.com>
2013-01-14 22:05:55 -0800

    Use iproj supplied configs and post-boot scripts.

--- device.mk
+++ device.mk
@@ -20,22 +20,6 @@ PRODUCT_COPY_FILES += \
 	device/lge/i_vzw/prebuilt/root/ueventd.iproj.rc:root/ueventd.iproj.rc \
 	device/lge/i_vzw/prebuilt/root/ueventd.rc:root/ueventd.rc
 
-PRODUCT_COPY_FILES += \
-	device/lge/i_vzw/prebuilt/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
-	device/lge/i_vzw/prebuilt/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
-	device/lge/i_vzw/prebuilt/init.qcom.mdm_links.sh:system/etc/init.qcom.mdm_links.sh
-
-# Configs
-PRODUCT_COPY_FILES += \
-	device/lge/i_vzw/configs/vold.fstab:system/etc/vold.fstab \
-	device/lge/i_vzw/configs/atcmd_virtual_kbd.kl:system/usr/keylayout/atcmd_virtual_kbd.kl \
-	device/lge/i_vzw/configs/ffa-keypad_qwerty.kl:system/usr/keylayout/ffa-keypad_qwerty.kl \
-	device/lge/i_vzw/configs/i_atnt-keypad.kl:system/usr/keylayout/i_atnt-keypad.kl \
-	device/lge/i_vzw/configs/pmic8058_pwrkey.kl:system/usr/keylayout/pmic8058_pwrkey.kl \
-	device/lge/i_vzw/configs/touch_dev.kl:system/usr/keylayout/touch_dev.kl \
-	device/lge/i_vzw/configs/touch_dev.idc:system/usr/idc/touch_dev.idc \
-	device/lge/i_vzw/configs/thermald.conf:system/etc/thermald.conf
-
 # Recovery (i_vzw specific)
 PRODUCT_COPY_FILES += \
 	device/lge/i_vzw/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
