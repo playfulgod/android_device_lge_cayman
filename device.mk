fbdc154025db26675a7811572854e79b63390496
Tom Marshall <tdm.code@gmail.com>
2013-01-15 12:08:49 -0800

    init: fold init.target.rc into init.iproj.rc to align with i_atnt.

--- device.mk
+++ device.mk
@@ -15,7 +15,6 @@ PRODUCT_COPY_FILES += \
 	device/lge/i_vzw/prebuilt/root/init.iproj.rc:root/init.iproj.rc \
 	device/lge/i_vzw/prebuilt/root/init.iproj.usb.rc:root/init.iproj.usb.rc \
 	device/lge/i_vzw/prebuilt/root/init.qcom.sh:root/init.qcom.sh \
-	device/lge/i_vzw/prebuilt/root/init.target.rc:root/init.target.rc \
 	device/lge/i_vzw/prebuilt/root/init.usb.rc:root/init.usb.rc \
 	device/lge/i_vzw/prebuilt/root/ueventd.iproj.rc:root/ueventd.iproj.rc \
 	device/lge/i_vzw/prebuilt/root/ueventd.rc:root/ueventd.rc
